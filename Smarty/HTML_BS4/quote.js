70c80038a89f313ca3be274bcf919217824eb945
Louis-Philip Desmarais Beaupré <l-pdesmarais.b@Louis-Philips-MacBook-Air.local>
2022-05-30 16:00:44 -0400

    creation de quote html et js et finalisation de indexhtml

---
+++ Smarty/HTML_BS4/quote.js
@@ -0,0 +1,42 @@
+
+			{/* FONCTIONS SELECTIONS
+				
+				
+				
+				$(document).ready(function(){
+						$("select").change(function(){
+							$( "select option:selected").each(function(){
+								if($(this).attr("value")=="red"){
+									$(".box").hide();
+									$(".red").show();
+								}
+								if($(this).attr("value")=="green"){
+									$(".box").hide();
+									$(".green").show();
+								}
+								if($(this).attr("value")=="blue"){
+									$(".box").hide();
+									$(".blue").show();
+								}
+							});
+						}).change();
+					}); */}
+
+					
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
